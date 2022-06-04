e6623e4812b0787eac24a582c4659d2698f7de91
juliorvm1 <juliorvm1@gmail.com>
2022-06-04 09:49:34 -0500

    se implementa la funcion de eliminar un registro des de la interfaz quitandolo de la lista qen la interfaz

--- static/main.js
+++ static/main.js
@@ -55,14 +55,27 @@ function renderUser(users){
           <header class="d-flex justify-content-between align-items-center">
             <h3>${user.username}</h3>
             <div>
-              <button class="btn btn-danger btn-sm">delete</button>
-              <button class="btn btn-secondary btn-sm">edit</button>
+              <button class="btn-delete btn btn-danger btn-sm">delete</button>
+              <button class="btn-edit btn btn-secondary btn-sm">edit</button>
             </div>
           </header>
             <p>${user.email}</p>
             <p class="text-truncate">${user.password}</p>
             `
         //console.log(userItem)
+        const btnDelete=userItem.querySelector('.btn-delete')
+        btnDelete.addEventListener('click', async ()=>{
+            const response= await fetch(`/api/users/${user.id}`,{
+                method:'DELETE'
+            })
+            const data=await response.json()
+            console.log(data)
+            //console.log(data)
+            //console.log(users)
+            users=users.filter(user => user.id !== data.id )
+            //console.log(users)
+            renderUser(users)
+        })
         userList.append(userItem)
     });
 }
